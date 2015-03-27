2fbacb675c802c86b6bd0ef881141efbcc3eaa95
felilondocho <pipes.20@hotmail.com>
2015-03-27 16:38:32 -0500

    Adicion rest

--- mvc/www/js/controllers.js
+++ mvc/www/js/controllers.js
@@ -1,23 +1,72 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope,$state) {
-		$scope.processConferencia = function(){
-		$state.go("tab.seleccionarevento")
+.controller('DashCtrl', function($scope,$state,$http) {
+		$scope.processConferencia = function(conferencia){
+		$http({
+  			method  : 'POST',
+  			url     : 'http://domain.com/webservice/conferenciarest',
+  			data    : {
+  						nombre: conferencia.nombre,
+        				nombrepersona: conferencia.nombrepersona,
+        				fecha: conferencia.fecha,
+        				numeroasistentes:conferencia.numeroasistentes
+  						},
+  			headers : { 'Content-Type': 'application/x-www-form-urlencoded' } 
+  		})
+      $state.go('tab.seleccionarevento')
 	};
 })
 
-.controller('SeleccionarEventoCtrl', function($scope,$state) {
-	$scope.processEvento = function(){
+.controller('SeleccionarEventoCtrl', function($scope,$state,$http) {
+	$scope.processEvento = function(evento){
+		$http({
+  		method  : 'POST',
+  		url     : 'http://domain.com/webservice/eventorest',
+  		data    : {
+  					nombre: evento.nombre,
+        			tipoevento: evento.tipo
+  					},
+  			headers : { 'Content-Type': 'application/x-www-form-urlencoded' } 
+  		})
 		$state.go("tab.dash")
 		console.log("Creado")
 	};
 })
 
-.controller('SillasCtrl', function($scope,$state) {
-	$scope.processSilla = function(){
+.controller('SillasCtrl', function($scope,$state,$http) {
+	$scope.processSilla = function(servicioconferencia, servicioevento){
+		//Obtener conferencias disponibles
+		$http.get("http://domain.com/webservice/conferenciasrest")
+    	.success(function(response){
+      $scope.servicioconferencia = response;
+      console.log(response);
+    });
+    	//Obtener eventos en las conferencias
+    $http.get("http://domain.com/webservice/eventosrest")
+    	.success(function(response){
+      $scope.servicioevento = response;
+      console.log(response);
+    });
+    	//enviar la informacion para comprar la silla
+	$http({
+  		method  : 'POST',
+  		url     : 'http://domain.com/webservice/sillarest',
+  		data    : {
+
+  			//		conferenciaseleccionada: servicioconferencia.conferencia,
+        	//		eventoseleccionado: servicioevento.evento
+  					},
+  			headers : { 'Content-Type': 'application/x-www-form-urlencoded' } 
+  		})
+
 		$state.go("tab.sillacomprada")
 	};
 })
 
-.controller('SillaCompradaCtrl', function($scope,$state) {
+.controller('SillaCompradaCtrl', function($scope,$state,$http) {
+	  $http.get("http://domain.com/webservice/sillasrest")
+    	.success(function(response){
+      $scope.servicio = response;
+      console.log(response);
+    });
 });
